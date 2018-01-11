ef2885f0723f3d1cdce43f776d8a9d9ba97d92ab
hallk1331 <hallk1331@FHC-IC0034079.wrdsb.ca>
2018-01-11 14:03:25 -0500

    Animation merged

--- core/src/gdx/menu/Screens/ScrAnimation.java
+++ core/src/gdx/menu/Screens/ScrAnimation.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import gdx.menu.GamMenu;
 
@@ -15,7 +17,12 @@ public class ScrAnimation implements Screen, InputProcessor {
     Button btnMenu, btnSign, btnPlay;
     GamMenu gamMenu;
     OrthographicCamera oc;
-    Texture txButtonM, txButtonQ, txNamT;
+    Texture txButtonM, txButtonQ, txNamT, txSheet;
+    Animation araniDude[];
+    TextureRegion trTemp;
+    int fW, fH, fSx, fSy;
+    int nFrame, nPos;
+    int nX = 100, nY = 100;
     Sprite sprButtonMenu, sprButtonSign, sprNamT, sprDude;
     SpriteBatch batch;
     
@@ -33,11 +40,32 @@ public class ScrAnimation implements Screen, InputProcessor {
         btnSign = new Button(100, 50, Gdx.graphics.getWidth() - 100, Gdx.graphics.getHeight() - 50, "SignB.png");
         btnPlay = new Button(100, 50, 0, Gdx.graphics.getHeight() - 50, "Play.jpg");
         txNamT = new Texture("A.jpg");
+        txSheet = new Texture("Vlad.png");
         sprNamT = new Sprite(txNamT);
         sprNamT.setFlip(false, true);
         sprNamT.setSize(60, 80);
         sprNamT.setPosition(Gdx.graphics.getWidth()/2 - 30, Gdx.graphics.getHeight()/2 - 40);
-                       
+        
+        
+        //Animation Stuff
+        nFrame = 0;
+        nPos = 0;
+        araniDude = new Animation[8];
+        fW = txSheet.getWidth() / 8;
+        fH = txSheet.getHeight() / 8;
+        for (int i = 0; i < 8; i++) {
+            Sprite[] arSprDude = new Sprite[8];
+            for (int j = 0; j < 8; j++) {
+                fSx = j * fW;
+                fSy = i * fH;
+                sprDude = new Sprite(txSheet, fSx, fSy, fW, fH);
+                sprDude.setFlip(false, true);
+                arSprDude[j] = new Sprite(sprDude);
+            }
+            araniDude[i] = new Animation(0.5f, arSprDude);
+
+        }
+        
         Gdx.input.setInputProcessor(this);
     }
 
@@ -45,8 +73,46 @@ public class ScrAnimation implements Screen, InputProcessor {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 1, 1, 1); //Cyan background.
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        //Animation Stuff
+        
+        if (nFrame > 7) {
+            nFrame = 0;
+        }
+        trTemp = araniDude[nPos].getKeyFrame(nFrame, false);
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            nX = nX-=1;
+            nPos = 7;
+            nFrame++;
+        } if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            nX = nX+=1;
+            nPos = 0;
+            nFrame++;
+        } if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            nY = nY-=1;
+            nPos = 1;
+            nFrame++;
+        } if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            nY = nY+=1;
+            nPos = 4;
+            nFrame++;
+        } if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.UP)){
+            nPos = 3;
+            nFrame--;
+        } if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            nPos = 6;
+            nFrame--;
+        } if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.UP)){
+            nPos = 2;
+            nFrame--;
+        } if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            nPos = 5;
+            nFrame--;
+        }
+        
         batch.begin();
         batch.setProjectionMatrix(oc.combined);
+        batch.draw(trTemp, nX, nY);
         btnMenu.draw(batch);
         btnSign.draw(batch);
         btnPlay.draw(batch);
